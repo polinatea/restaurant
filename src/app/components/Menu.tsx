2eefd667d57c4ae9c495b35a13764343dcab9a77
polinatea <polinachainikova99@gmail.com>
2023-10-25 12:07:35 +0300

    fix: in dropdown menu login,card deleted; gap added

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -21,15 +21,10 @@ const Menu = () => {
         <Image src="/menu-button-close.png" alt="" width={20} height={20} onClick={()=>setOpen(false)}/>
         )}
       {open &&(
-        <div className='bg-yellow-500 text-white absolute left-0 top-12 w-full h-[calc(100vh-3rem)] flex flex-col items-center text-3xl z-10'>
+        <div className='bg-yellow-500 text-white absolute left-0 top-12 w-full h-[calc(100vh-3rem)] flex flex-col items-center text-3xl z-10 gap-1'>
         {links.map(item=>(
           <Link href={item.url} key={item.id} onClick={()=>setOpen(false)}>{item.title}</Link>
         ))} 
-        {!user ? <Link href="/login" onClick={()=>setOpen(false)}>Login</Link>:
-        <Link href="/orders" onClick={()=>setOpen(false)}>Orders</Link>}
-        <Link href="/cart" onClick={()=>setOpen(false)}>
-          <CartIcon />
-        </Link>
       </div>
       )}
 
